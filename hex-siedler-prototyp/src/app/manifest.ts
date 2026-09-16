56f6c70cc02efedb4c4537ef33af167fff5f35e9
TimoLaux1 <timo.laux@web.de>
2026-09-16 23:53:24 +0200

    Add files via upload

--- hex-siedler-prototyp/src/app/manifest.ts
+++ hex-siedler-prototyp/src/app/manifest.ts
@@ -9,7 +9,7 @@ export default function manifest(): MetadataRoute.Manifest {
     display: "standalone",
     background_color: "#f7f2e7",
     theme_color: "#1f6b57",
-    orientation: "any",
+    orientation: "landscape",
     icons: [
       { src: "/icon-192.png", sizes: "192x192", type: "image/png" },
       { src: "/icon-512.png", sizes: "512x512", type: "image/png" },
